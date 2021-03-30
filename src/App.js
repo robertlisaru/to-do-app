2cd25e33e83c3646a0dd151c30bdc0aaf4266256
Lisaru Robert <lisaru.robert@hotmail.com>
2021-03-30 12:56:43 +0300

    Implement create todo functionality

--- src/App.js
+++ src/App.js
@@ -5,10 +5,18 @@ import Todos from './todos'
 
 function App() {
   const [todos, setTodos] = useState({ data: [], isLoading: true })
+  const [newTodoTitle, setNewTodoTitle] = useState('')
 
   useEffect(() => {
     Todos.getAll().then((response) => setTodos({ data: response.data, isLoading: false }))
   }, [])
+
+  const createTodo = () => {
+    Todos.create({ title: newTodoTitle }).then(() => {
+      Todos.getAll().then((response) => setTodos({ data: response.data, isLoading: false }))
+      setNewTodoTitle('')
+    })
+  }
 
   return (
     <div className="App">
@@ -17,7 +25,15 @@ function App() {
       </header>
 
       {todos.isLoading ? <p>Fetching todos...</p> :
-        <TodosList todos={todos.data} />
+        (
+          <>
+            <input value={newTodoTitle} onChange={(event) => { setNewTodoTitle(event.target.value) }}
+              type='text'
+              placeholder='What needs to be done' />
+            <button onClick={createTodo}>Add</button>
+            <TodosList todos={todos.data} />
+          </>
+        )
       }
 
     </div>
--- src/todos.js
+++ src/todos.js
@@ -3,12 +3,20 @@ import axios from 'axios'
 const TODOS_LIST_URL = "https://todo-backend-hanami.herokuapp.com/"
 
 function getAll() {
-
     return axios.get(TODOS_LIST_URL, {
         headers: {
-            Accept: 'application/json',
+            'Accept': 'application/json',
         }
     })
 }
 
-export default { getAll }
+function create(todo) {
+    return axios.post(TODOS_LIST_URL, todo, {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+}
+
+export default { getAll, create }
