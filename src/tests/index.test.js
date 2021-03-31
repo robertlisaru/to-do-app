7a3ac2ea34ec90654f3aa9f482296fa1ffb7f6e5
Lisaru Robert <lisaru.robert@hotmail.com>
2021-03-31 20:05:26 +0300

    Test if it renders all todos

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,37 +1,9 @@
 import App from '../App.js'
 import { render, unmountComponentAtNode } from "react-dom"
 import { act } from "react-dom/test-utils"
+import Todos from '../todos'
 
-jest.mock("../todos", () => {
-    return {
-        getAll: () => {
-            return Promise.resolve([
-                {
-                    "id": 1,
-                    "title": "buy milk",
-                    "completed": false,
-                    "order": 0,
-                    "url": "https://todo-backend-hanami.herokuapp.com/todos/1"
-                },
-                {
-                    "id": 2,
-                    "title": "adjust moon orbit",
-                    "completed": false,
-                    "order": 0,
-                    "url": "https://todo-backend-hanami.herokuapp.com/todos/2"
-                },
-                {
-                    "id": 3,
-                    "title": "sleep",
-                    "completed": false,
-                    "order": 0,
-                    "url": "https://todo-backend-hanami.herokuapp.com/todos/3"
-                }
-            ])
-        }
-    }
-
-})
+jest.mock('../todos')
 
 let container = null
 beforeEach(() => {
@@ -47,16 +19,43 @@ afterEach(() => {
     container = null
 })
 
-it("should render all todos", () => {
-    act(() => {
-        render(
-            <App />,
-            container
-        )
+it("should render all todos", async () => {
+    const todos = [
+        {
+            "id": 1,
+            "title": "buy milk",
+            "completed": false,
+            "order": 0,
+            "url": "https://todo-backend-hanami.herokuapp.com/todos/1"
+        },
+        {
+            "id": 2,
+            "title": "adjust moon orbit",
+            "completed": false,
+            "order": 0,
+            "url": "https://todo-backend-hanami.herokuapp.com/todos/2"
+        },
+        {
+            "id": 3,
+            "title": "sleep",
+            "completed": false,
+            "order": 0,
+            "url": "https://todo-backend-hanami.herokuapp.com/todos/3"
+        },
+        {
+            "id": 4,
+            "title": "rest",
+            "completed": true,
+            "order": 0,
+            "url": "https://todo-backend-hanami.herokuapp.com/todos/4"
+        }
+    ]
+    const resp = { data: todos }
+    Todos.getAll.mockImplementation(() => Promise.resolve(resp))
+
+    await act(async () => {
+        render(<App />, container)
     })
 
-    expect(
-        container.querySelectorAll("[data-test='todo']").length
-    ).toEqual(3)
-
+    expect(container.querySelectorAll("[data-test='todo']").length).toEqual(4)
 })
