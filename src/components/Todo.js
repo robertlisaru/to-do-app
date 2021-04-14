07586f9ab7483a582db265dacba2a42f013abf3f
Lisaru Robert <lisaru.robert@hotmail.com>
2021-04-14 10:43:38 +0300

    Display delete button

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,7 @@
 import PropTypes from 'prop-types'
 import Checkbox from '@material-ui/core/Checkbox'
+import DeleteIcon from '@material-ui/icons/Close'
+import IconButton from '@material-ui/core/IconButton'
 import Todos from './../todos'
 
 const Todo = ({ todo, notifyChange }) => {
@@ -19,6 +21,9 @@ const Todo = ({ todo, notifyChange }) => {
                 data-testid="todoLabel">
                 {todo.completed ? <s>{todo.title}</s> : todo.title}
             </div>
+            <IconButton aria-label="delete" className='deleteButton'>
+                <DeleteIcon />
+            </IconButton>
         </li>
     )
 }
